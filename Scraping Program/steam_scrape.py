d2348519c44a546bb1d2f2186203d9664c370f91
mrhatman26 <{ID}+{username}+@user.noreply.github.com>
2025-06-25 20:24:41 +0100

    Started Steam scraping program

---
+++ Scraping Program/steam_scrape.py
@@ -0,0 +1,22 @@
+import requests, csv, time
+from bs4 import BeautifulSoup as bs
+
+base_url = "https://store.steampowered.com/"
+request_url = "https://store.steampowered.com/search/"
+main_page = requests.get(request_url)
+main_soup = bs(main_page.content, "html.parser")
+game_data = []
+max_game_no = 10
+current_game_no = 0
+
+def get_page_data():
+    pass
+
+def save_game_data():
+    pass
+
+while current_game_no < max_game_no:
+    print(str(current_game_no) + "/" + str(max_game_no) + " games.")
+    print("Next URL is:\n" + request_url + "\n")
+    print(main_soup.prettify())
+    break
