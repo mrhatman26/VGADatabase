7d3748f6d11e6ac07788b15febcf1659ff8eee48
mrhatman26 <{ID}+{username}+@user.noreply.github.com>
2025-06-26 21:57:41 +0100

    More notes and some temp files

--- Scraping Program/steam_scrape.py
+++ Scraping Program/steam_scrape.py
@@ -1,12 +1,12 @@
-import requests, csv, time, pyperclip
+import requests, csv, time, pyperclip, json
 from bs4 import BeautifulSoup as bs
 
 base_url = "https://store.steampowered.com/"
-infite_url = "https://store.steampowered.com/search/results/?query&start=50&count=50&dynamic_data=&sort_by=_ASC&supportedlang=english&snr=1_7_7_230_7&infinite=1"
-request_url = "https://store.steampowered.com/search/"
+infite_url = "https://store.steampowered.com/search/results/?query&start=50&count=1&dynamic_data=&sort_by=_ASC&supportedlang=english&snr=1_7_7_230_7&infinite=1"
+request_url = "https://store.steampowered.com/search/?term="
 game_data = []
 max_game_no = 500
-current_game_no = 0
+current_game_no = 50
 
 def get_page_data():
     pass
@@ -15,13 +15,16 @@ def save_game_data():
     pass
 
 def new_infite_url(game_no):
-    return "https://store.steampowered.com/search/results/?query&start=" + str(game_no) + "&count=" + str(game_no) + "&dynamic_data=&sort_by=_ASC&supportedlang=english&snr=1_7_7_230_7&infinite=1"
+    return "https://store.steampowered.com/search/results/?query&start=" + str(game_no) + "&count=50&dynamic_data=&sort_by=_ASC&supportedlang=english&snr=1_7_7_230_7&infinite=1"
 
-def error_check(main_soup):
-    pyperclip.copy(main_soup)
+def error_check(main_soup, e):
+    e = e.json()
+    pyperclip.copy(main_soup.prettify())
     for title in main_soup.find_all("title"):
         if title.text == "Site Error":
             raise Exception("Steam returned 'Site Error' page and so the search page has no been loaded. Page HTML saved to clipboard")
+    with open("json.json", "w", encoding="utf-8") as blah:
+        json.dump(e, blah, ensure_ascii=False, indent=4)
     raise Exception("No games found. Page HTML saved to clipboard.")
 
 while current_game_no < max_game_no:
@@ -31,7 +34,7 @@ while current_game_no < max_game_no:
     main_soup = bs(main_page.content, "html.parser")
     all_games = main_soup.find_all("a", attrs={"class": "search_result_row ds_collapse_flag"})
     if len(all_games) < 1:
-        error_check(main_soup)
+        error_check(main_soup, main_page)
     for item in all_games:
         print(item.find_all("span", attrs={"class": "title"})[0].text)
         current_game_no += 1
